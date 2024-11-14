0c2ce8b916460ef072e67da7d653c30bc52d526c
Danny <34337548+DannyHD001@users.noreply.github.com>
2024-11-14 10:01:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base:  '/ledcalc/'
   plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true,
-    host: '0.0.0.0',
-    hmr: {
-      protocol: 'ws',
-      host: '0.0.0.0',
-      port: 5173
-    }
-  },
-  preview: {
-    port: 5173,
-    strictPort: true,
-    host: true
-  }
-});
+})
