38bc8be5bed8ade399138450f93592fc92dd5f46
Danny <34337548+DannyHD001@users.noreply.github.com>
2024-11-17 13:24:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ledcalc/', // Replace with your GitHub repository name
+  base: 'ledcalc', // Replace with your GitHub repository name
   plugins: [react()],
 })
